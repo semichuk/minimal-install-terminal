dfee119700ca7cd50a0ac3ebf280fbd534cbe03a
kisemichuk <70934055+semichuk@users.noreply.github.com>
2023-11-09 09:49:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 echo "//////////////////////minimal install terminal//////////////////////"
 sudo apt-get update
-sudo apt-get install zsh konsole zip unzip -y
+sudo apt-get install zsh  -y
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
-cp "$(pwd)/ki-konsole-theme.colorscheme" /home/$USER/.local/share/konsole
 cp "$(pwd)/ki.zsh-theme" /home/$USER/.oh-my-zsh/themes/
 cp "$(pwd)/.zshrc" /home/$USER/
 echo "//////////////////////installation is finished//////////////////////"

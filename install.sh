e9c28d8bbc49aaba2c122d337925c7c4e99de7e6
kisemichuk <70934055+semichuk@users.noreply.github.com>
2025-04-12 04:48:57 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "//////////////////////minimal install terminal//////////////////////"
 sudo apt install zsh -y
 sudo dnf install zsh -y
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
-cp "$(pwd)/ki.zsh-theme" /home/$USER/.oh-my-zsh/themes/
-cp "$(pwd)/.zshrc" /home/$USER/
+cp "$(pwd)/ki.zsh-theme" ~/.oh-my-zsh/themes/
+cp "$(pwd)/.zshrc" ~/
 exec zsh
 echo "//////////////////////installation is finished//////////////////////"
